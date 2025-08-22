aecc56475f3bbd91495f34bdcae586d4b199a9a5
Agis Theodoropoulos <agis@eureton.com>
2025-08-22 18:41:37 +0300

    Composition scaffold

--- src/workflow/core.clj
+++ src/workflow/core.clj
@@ -1,6 +1,36 @@
-(ns workflow.core)
+(ns workflow.core
+  (:require [clojure.string :as string]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn fn->str [f]
+  (let [match #(re-find #"^(.*)@" %)]
+    (-> f
+        (str)
+        (match)
+        (second)
+        (string/replace \$ \/)
+        (string/replace \_ \-))))
+
+(defn run-step [step env]
+  (let [label (fn->str step)
+        result (try
+                 [label :ok (step env)]
+                 (catch Exception e
+                   [label :error e]))]
+    (-> env
+        (assoc :ok? (-> result (get 1) (= :ok)))
+        (update :results conj result))))
+
+(defn- embellish [step]
+  (fn [env]
+    (cond->> env
+      (:ok? env) (run-step step))))
+
+(defn make [& steps]
+  (fn [params]
+    (let [f (->> steps
+                 (reverse)
+                 (map embellish)
+                 (apply comp))]
+      (f {:params params
+          :ok? true
+          :results []}))))
