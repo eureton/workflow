ec4eb68938a7b84aa9ee38f0674da5f02f8f34b7
Agis Theodoropoulos <agis@eureton.com>
2025-08-22 18:42:23 +0300

    Fix license URL

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Idiomatic ROP-based workflow DSL in Clojure."
   :url "https://github.com/eureton/workflow"
   :license {:name "MIT"
-            :url "https://github.com/eureton/effective/blob/master/LICENSE"}
+            :url "https://github.com/eureton/workflow/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns workflow.core})
