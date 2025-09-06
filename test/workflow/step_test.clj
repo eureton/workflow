c3ec4be849267e74507d304c22a12584a693c493
Agis Theodoropoulos <agis@eureton.com>
2025-09-06 17:31:48 +0300

    Test inline destructuring

--- test/workflow/step_test.clj
+++ test/workflow/step_test.clj
@@ -1,7 +1,7 @@
 (ns workflow.step-test
   (:require [clojure.test :refer [deftest is]]
             [workflow.core :as wf]
-            [workflow.step :refer [defstep]]))
+            [workflow.step :refer [defstep |=|]]))
 
 (defstep calculate-x [w]
   (+ w 1))
@@ -21,3 +21,13 @@
     (is (=   4 x))
     (is (=  14 y))
     (is (= 114 z))))
+
+(deftest destructure-inline
+  (let [workflow (wf/make
+                   [:calculate-x (|=| [w] (* w 2)) :x]
+                   [:calculate-y (|=| [x] (* x 3)) :y]
+                   [:calculate-z (|=| [y] (* y 3)) :z])
+        {:keys [x y z]} (-> {:w 3} workflow :temp)]
+    (is (=  6 x))
+    (is (= 18 y))
+    (is (= 54 z))))
